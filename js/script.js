b3cc7d46fcd00fcf6ab2c8b119a02986e7d2b40b
Goraychenok <55753151+Goraychenok@users.noreply.github.com>
2020-10-27 01:00:13 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+      },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
