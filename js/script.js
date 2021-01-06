f7f3ec25f4a303aa66f723b6593d6b9845b0bc54
Goraychenok <55753151+Goraychenok@users.noreply.github.com>
2021-01-07 04:29:01 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,55 +1,20 @@
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-      },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-
-  jQuery(document).ready(function() {
+jQuery(document).ready(function() {
 	$('.header__burger').click(function(event) {
 		$('.header__burger,.header__menu').toggleClass('active');
         $('body').toggleClass('lock');
-        $('.header').addClass('active');
-
-        
+        $('.header').addClass('active'); 
     });
-    $(".popup").delay(7000).fadeIn();
-    
-});
-function slowScroll(id){
-	var offset = 25;
-	$('html,body').animate ({
-		scrollTop: $(id).offset ().top - offset
-	},1000);
-	$('body').removeClass('lock');
-	$('.header__burger,.header__menu').removeClass('active');
-	return false;
-}
-
-//phone mask
-jQuery(function($){
-  $("#phone").mask("+7 (999) 999-99-99");
-  $("#phone-mask").mask("+7 (999) 999-99-99");
-  $("#phone-mask-two").mask("+7 (999) 999-99-99");
 });
 
+$('#button_one').click(function(event){
+    $('.companys-two').addClass('block');
+    $('#button_two').addClass('block');
+    $('#button_one').addClass('none');
 
-// $(document).mouseup(function (e){
-//   var popups = $(".popup"); 
-//   var popupBtn = $(".popup-btn");
-//   if (popupBtn.is(e.target)) { 
-//       popups.addClass('none');
-//   } else if(!popupBtn.is(e.target)){
-//      popups.delay(1000).fadeIn();
-//   }
-// });
+});
 
-
-
-
+$('#button_two').click(function(event){
+    $('.companys-two').removeClass('block');
+    $('#button_two').removeClass('block');
+    $('#button_one').removeClass('none');
+});
